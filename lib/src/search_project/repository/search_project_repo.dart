b32df07f2f788ffbcd553a73862892112b3b9608
Umesh <globaltechumesh11@gmail.com>
2025-02-04 17:13:17 +0545

    End Point

--- lib/src/search_project/repository/search_project_repo.dart
+++ lib/src/search_project/repository/search_project_repo.dart
@@ -6,7 +6,7 @@ class ProjectSearchRepository {
 
   static Future<List<ProjectModel>> searchProjects({required String query}) async {
     try {
-      //To run the query
+      //End point
       String api = "/project/search?name=$query";
       var response = await apiProvider.getAPI(endPoint: api);
       ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(response);
