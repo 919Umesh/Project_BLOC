f71a997647aef2b1d31a5f00b645571c93fb488d
Umesh <globaltechumesh11@gmail.com>
2025-02-05 13:10:25 +0545

    Decode

--- lib/src/project_list/repository/project_list_repo.dart
+++ lib/src/project_list/repository/project_list_repo.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:project_bloc/src/project_list/model/project_list_model.dart';
 import '../../../../core/core.dart';
@@ -6,16 +9,20 @@ class ProjectListRepository {
   static Future<List<ProjectModel>> getProjectList(
       {required String status}) async {
     try {
-      Fluttertoast.showToast(msg: 'dgfh');
       String api = "/project/get?status=$status";
       var response = await apiProvider.getAPI(endPoint: api);
-      ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(response);
+      // ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(response);
+      var responseJson = jsonDecode(response);
+      ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(responseJson);
       if (projectResponse.status == 200) {
         return projectResponse.projects;
       } else {
+
         throw Exception(projectResponse.message);
       }
     } catch (e) {
+      debugPrint('-------------dgbdg-------');
+      debugPrint(e.toString());
       throw Exception(e.toString());
     }
   }
