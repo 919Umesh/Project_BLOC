49b6086a4d78728a2542f8442783aad5bd30ec00
Umesh <globaltechumesh11@gmail.com>
2025-02-05 13:13:05 +0545

    Default

--- lib/src/project_list/repository/project_list_repo.dart
+++ lib/src/project_list/repository/project_list_repo.dart
@@ -1,7 +1,3 @@
-import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:project_bloc/src/project_list/model/project_list_model.dart';
 import '../../../../core/core.dart';
 
@@ -11,18 +7,13 @@ class ProjectListRepository {
     try {
       String api = "/project/get?status=$status";
       var response = await apiProvider.getAPI(endPoint: api);
-      // ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(response);
-      var responseJson = jsonDecode(response);
-      ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(responseJson);
+      ProjectResponseModel projectResponse = ProjectResponseModel.fromJson(response);
       if (projectResponse.status == 200) {
         return projectResponse.projects;
       } else {
-
         throw Exception(projectResponse.message);
       }
     } catch (e) {
-      debugPrint('-------------dgbdg-------');
-      debugPrint(e.toString());
       throw Exception(e.toString());
     }
   }
