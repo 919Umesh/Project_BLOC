7bad88d22fd3ef04041a1f6562faca1a9567ffeb
Umesh <globaltechumesh11@gmail.com>
2025-02-06 12:38:36 +0545

    Event

--- lib/src/create_product/bloc/create_product_event.dart
+++ lib/src/create_product/bloc/create_product_event.dart
@@ -1,22 +1,4 @@
-// import 'package:dio/dio.dart';
-// import 'package:equatable/equatable.dart';
-//
-// abstract class CreateProductEvent extends Equatable {
-//   const CreateProductEvent();
-//
-//   @override
-//   List<Object> get props => [];
-// }
-//
-// class CreateProductRequested extends CreateProductEvent {
-//
-//   final FormData formData;
-//
-//   const CreateProductRequested({required this.formData});
-//
-//   @override
-//   List<Object> get props => [formData];
-// }
+
 
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
