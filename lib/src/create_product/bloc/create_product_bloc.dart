fdc0cf8025e61d191d4a018c0097a00c59529f60
Umesh <globaltechumesh11@gmail.com>
2025-02-06 16:38:30 +0545

    Bloc

--- lib/src/create_product/bloc/create_product_bloc.dart
+++ lib/src/create_product/bloc/create_product_bloc.dart
@@ -13,16 +13,15 @@ class CreateProductBloc extends Bloc<CreateProductEvent, CreateProductState> {
     on<CreateProductRequested>(_onProductCreateRequested);
   }
 
-  Future<void> _onProductCreateRequested(
-      CreateProductRequested event, Emitter emit) async {
+  Future<void> _onProductCreateRequested(CreateProductRequested event, Emitter emit) async {
     emit(CreateProductLoading());
     try {
       final response = await _createProductRepository.createProduct(form: event.formData);
       if (response.statusCode == 200 || response.statusCode == 201) {
-        emit( CreateProductSuccess(message: response.statusCode!.toString()));
+        emit(CreateProductSuccess(message: response.statusCode!.toString()));
       } else {
         Fluttertoast.showToast(msg: 'Failure');
-        emit( CreateProductError(message: response.statusCode!.toString()));
+        emit(CreateProductError(message: response.statusCode!.toString()));
       }
     } catch (e) {
       emit(CreateProductError(message: e.toString()));
--- lib/src/create_product/bloc/create_product_state.dart
+++ lib/src/create_product/bloc/create_product_state.dart
@@ -28,3 +28,36 @@ class CreateProductError extends CreateProductState {
   @override
   List<Object> get props => [message];
 }
+
+// import 'package:equatable/equatable.dart';
+// import '../../project_list/model/project_list_model.dart';
+//
+// abstract class CreateProductState extends Equatable {
+//   const CreateProductState();
+//
+//   @override
+//   List<Object> get props => [];
+// }
+//
+// class CreateProductStateInitial extends CreateProductState {}
+//
+// class CreateProductStateLoading extends CreateProductState {}
+//
+// class CreateProductSuccess extends CreateProductState {
+//   final List<ProjectModel> projects;
+//
+//   const CreateProductSuccess({required this.projects});
+//
+//   @override
+//   List<Object> get props => [projects];
+// }
+//
+// class CreateProductFailure extends CreateProductState
+// {
+//   final String errorMessages;
+//
+//   const CreateProductFailure({required this.errorMessages});
+//
+//   @override
+//   List<Object> get props => [errorMessages];
+// }
