b14cc705be497e16c14166f72f25700782c76e96
Umesh <globaltechumesh11@gmail.com>
2025-02-07 17:07:46 +0545

    Screen

--- lib/src/project_list/bloc/project_list_bloc.dart
+++ lib/src/project_list/bloc/project_list_bloc.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 import '../model/project_list_model.dart';
 import '../repository/project_list_repo.dart';
 part 'project_list_state.dart';
