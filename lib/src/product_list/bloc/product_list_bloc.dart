82db1ced506a8f1cb1c3d8ae128c8db50f6c0fea
Umesh <globaltechumesh11@gmail.com>
2025-02-10 17:16:29 +0545

    Product List

--- lib/src/product_list/bloc/product_list_bloc.dart
+++ lib/src/product_list/bloc/product_list_bloc.dart
@@ -1,41 +1,3 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import '../product_list.dart';
-//
-// class ProductListBloc extends Bloc<ProductListEvent, ProductListState> {
-//
-//   final List<ProductModel> _allProducts = [];
-//
-//   ProductListBloc() : super(ProductListInitial()) {
-//     on<ProductListRequested>(_onProductListRequested);
-//   }
-//
-//   Future<void> _onProductListRequested(
-//       ProductListRequested event,
-//       Emitter<ProductListState> emit,
-//       ) async {
-//     try {
-//       if (event.page == 1) {
-//         emit(ProductListLoading());
-//         _allProducts.clear();
-//       }
-//
-//       final newProducts = await ProductListRepository.getProductList(
-//         page: event.page,
-//         limit: event.limit,
-//       );
-//
-//       _allProducts.addAll(newProducts);
-//
-//       emit(ProductListSuccess(
-//         products: List.from(_allProducts),
-//         hasReachedEnd: newProducts.length < event.limit,
-//       ));
-//     } catch (e) {
-//       emit(ProductListFailure(errorMessage: e.toString()));
-//     }
-//   }
-// }
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../product_list.dart';
 
@@ -86,9 +48,7 @@ class ProductListBloc extends Bloc<ProductListEvent, ProductListState> {
           page: _currentPage,
           limit: _itemsPerPage,
         );
-
         _allProducts.addAll(newProducts);
-
         emit(ProductListSuccess(
           products: List.from(_allProducts),
           hasReachedEnd: newProducts.length < _itemsPerPage,
--- lib/src/product_list/bloc/product_list_event.dart
+++ lib/src/product_list/bloc/product_list_event.dart
@@ -1,20 +1,3 @@
-// import 'package:equatable/equatable.dart';
-//
-// abstract class ProductListEvent extends Equatable {
-//   const ProductListEvent();
-//   @override
-//   List<Object> get props => [];
-// }
-//
-// class ProductListRequested extends ProductListEvent {
-//   final int page;
-//   final int limit;
-//
-//   const ProductListRequested({required this.page, required this.limit});
-//
-//   @override
-//   List<Object> get props => [page, limit];
-// }
 abstract class ProductListEvent {}
 
 class ProductListRequested extends ProductListEvent {}
