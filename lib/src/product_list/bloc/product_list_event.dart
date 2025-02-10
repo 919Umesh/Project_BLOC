37efc92278bd4550a1a1861c705b533573fd894d
Umesh <globaltechumesh11@gmail.com>
2025-02-10 14:35:32 +0545

    Clear

--- lib/src/product_list/bloc/product_list_event.dart
+++ lib/src/product_list/bloc/product_list_event.dart
@@ -1,21 +1,3 @@
-// import 'package:equatable/equatable.dart';
-//
-// abstract class ProductListEvent extends Equatable {
-//
-//   const ProductListEvent();
-//
-//   @override
-//   List<Object> get props => [];
-// }
-//
-// class ProductListRequested extends ProductListEvent {
-//   final String page;
-//   final String limit;
-//   const ProductListRequested({required this.page,required this.limit});
-//
-//   @override
-//   List<Object> get props => [page,limit];
-// }
 import 'package:equatable/equatable.dart';
 
 abstract class ProductListEvent extends Equatable {
