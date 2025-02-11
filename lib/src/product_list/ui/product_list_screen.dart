0ec30c20deddb531adf5a63cb74da6e98c3b0345
Umesh <globaltechumesh11@gmail.com>
2025-02-11 15:42:59 +0545

    Pagination

--- lib/src/product_list/ui/product_list_screen.dart
+++ lib/src/product_list/ui/product_list_screen.dart
@@ -9,6 +9,8 @@ import '../bloc/product_list_event.dart';
 import '../bloc/product_list_state.dart';
 import '../model/product_list_model.dart';
 
+
+//Pagination Screen
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
 
