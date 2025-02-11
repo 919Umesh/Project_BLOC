f65eb8e8535beaaa6708f0379c973c688f735483
Umesh <globaltechumesh11@gmail.com>
2025-02-11 14:51:17 +0545

    Screen

--- lib/src/product_list/ui/product_list_screen.dart
+++ lib/src/product_list/ui/product_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:project_bloc/app/app.dart';
 import 'package:project_bloc/src/product_list/ui/product_details.dart';
 import 'package:shimmer/shimmer.dart';
