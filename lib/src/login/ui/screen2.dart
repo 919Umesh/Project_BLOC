2502013b9e24648b03f030159ebdd2f101bc0167
Umesh <globaltechumesh11@gmail.com>
2025-02-12 12:05:51 +0545

    Render

--- lib/src/login/ui/screen2.dart
+++ lib/src/login/ui/screen2.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:project_bloc/src/login/bloc/login_bloc.dart';
 
 class Screen2 extends StatefulWidget {
@@ -12,25 +14,47 @@ class Screen2 extends StatefulWidget {
 class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LoginBloc, LoginState>(
+    return BlocConsumer<LoginBloc, LoginState>(
       listener: (context, state) {
-        if (state is LoginSuccess) {
-        } else if (state is LoginError) {}
-      },
-      child: BlocBuilder<LoginBloc, LoginState>(
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Title'),
-            ),
-            body: const Center(
-              child: Column(
-                children: [Text('data1')],
-              ),
-            ),
+        if (state is LoginError) {
+          const Center(
+            child: Text('Some error occurred'),
           );
-        },
-      ),
+        }
+        if (state is LoginSuccess) {
+          Fluttertoast.showToast(msg: "Success");
+          //Navigate
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Product List'),
+          ),
+          body: Column(
+            children: [
+              Text('Data1'),
+              Text('Data1'),
+              Text('Data1'),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {},
+              icon: Icon(Bootstrap.house_add),
+              label: Text('Add Product')),
+        );
+      },
     );
   }
 }
+
+//
+// const Card(
+// child: Column(
+// children: [ListTile(
+// title: Text('Product Name'),
+// subtitle: Text('Sub Group'),
+// leading: Icon(Bootstrap.ladder),
+// )],
+// ),
+// );
