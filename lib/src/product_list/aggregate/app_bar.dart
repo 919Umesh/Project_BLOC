f9a404d88fef084a4e39af0c9cabf00a014c5dd1
Umesh <globaltechumesh11@gmail.com>
2025-02-21 12:01:05 +0545

    Appbar

---
+++ lib/src/product_list/aggregate/app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppBarPage extends StatefulWidget {
+  const AppBarPage({super.key});
+
+  @override
+  State<AppBarPage> createState() => _AppBarPageState();
+}
+
+class _AppBarPageState extends State<AppBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Text('bODY'),
+      ),
+    );
+  }
+}
